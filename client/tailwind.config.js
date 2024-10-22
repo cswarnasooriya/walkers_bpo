2bc4680aec39682ccad20006d0ffe8dcda591b3c
Layani-Wickramaarachchi <layanianuththara20@gmail.com>
2024-10-22 16:43:45 -0700

    Finance Section Updated

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
@@ -23,5 +25,6 @@ export default {
       },
     },
   },
-  plugins: [],
-};
+ 
+  }
+
