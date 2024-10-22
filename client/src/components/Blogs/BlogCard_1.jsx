b788f5ff2b20aff823b93e1ab8529ddc4d404329
Layani-Wickramaarachchi <layanianuththara20@gmail.com>
2024-10-22 15:15:31 -0700

    Changes added

---
+++ client/src/components/Blogs/BlogCard_1.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+
+const BlogCard_1 = ({ image, date, title, description, author }) => {
+  return (
+    <>
+      <Link
+        to={`/finance/${title}`}
+        onClick={() => {
+          window.scrollTo(0, 0);
+          // window.scroll({
+          //   top: 0,
+          //   left: 0,
+          //   behavior: "smooth",
+          // });
+        }}
+        state={{ image, date, title, description, author }}
+      >
+        <div className="p-4 shadow-lg transition-all duration-500 hover:shadow-xl dark:bg-slate-950 dark:text-white rounded-2xl">
+          <div className="overflow-hidden">
+            <img
+              src={image}
+              alt="No image"
+              className="mx-auto h-[400px] w-full object-cover transition duration-700 hover:skew-x-2 hover:scale-110 rounded-lg"
+            />
+          </div>
+          <div className="flex justify-between pt-2 text-slate-600">
+            <p>Since {date}</p>
+            <p className="line-clamp-1"> {author}</p>
+          </div>
+          <div className="space-y-2 py-3">
+            <h1 className="line-clamp-1 font-bold text-xl">{title}</h1>
+           
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default BlogCard_1;
---
+++ client/src/pages/Finance.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Finance = () => {
+  return (
+    <div className="pt-[200px]">Here from Finance page</div>
+  )
+}
+
+export default Finance
