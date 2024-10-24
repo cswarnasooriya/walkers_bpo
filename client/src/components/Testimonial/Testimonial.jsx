e47112fa82a885c798ca40b2a3ee3019bdad1586
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-10-24 15:55:27 +0530

    contracts changes

--- client/src/components/Testimonial/Testimonial.jsx
+++ client/src/components/Testimonial/Testimonial.jsx
@@ -65,35 +65,35 @@ const Testimonial = () => {
       <div data-aos="fade-up" data-aos-duration="300" className="py-2">
         <div className="container">
           {/* Header section */}
-          <div className="text-left mb-20 max-w-[1200px] mx-auto">
-            <p className="my-8 border-l-8 border-primary/80 py-2 pl-2 text-3xl font-bold font-serif md:text-4xl">
+          <div className=" mb-4 max-w-[1400px] mx-auto">
+            <p className="my-4 container border-l-8 border-primary/80 py-4 pl-8 text-4xl md:text-5xl font-semibold font-mono">
               <b>Our Values</b>
             </p>
-            <h1 className="text-center text-3xl md:text-5xl font-serif font-bold pt-4">Testimonial</h1>
-            <p className="text-lg text-center font-serif pt-8 text-gray-600">
+            
+            <p className="text-lg text-justify font-serif pt-2 text-gray-700">
               {" "}
-              Walkers Sons & Company Engineers (Pvt) Ltd is a subsidiary of Walkers Group, a major provider of technology, engineering, construction and manufacturing services conglomerate with global operations.
+              Walkers Sons & Company Engineers (Pvt) Ltd is a subsidiary of Walkers Group, a major provider of technology, engineering, construction and manufacturing services conglomerate with global operations.Walkers Sons & Company Engineers (Pvt) Ltd is a subsidiary of Walkers Group, a major provider of technology, engineering, construction and manufacturing services conglomerate with global operations.
             </p>
           </div>
           {/* testimonial section */}
           <div
-            data-aos="zoom-in"
-            data-aos-duration=""
-            className="grid grid-cols-1 max-w-[1200px] mx-auto gap-4 p-8"
+            data-aos=""
+            data-aos-duration="500"
+            className="grid grid-cols-1 max-w-[1400px] mx-auto gap-8 p-2 mb-14"
           >
             <Slider {...settings}>
               {testimonialData.map(({ id, name, text, img }) => {
                 return (
-                  <div key={id} className="my-8">
-                    <div className="flex flex-col justify-center items-center gap-4 text-center shadow-lg p-4 mx-4 rounded-xl dark:bg-gray-800 bg-primary/10 relative">
+                  <div key={id} className="my-4">
+                    <div className="flex flex-col justify-center items-center gap-4 text-center shadow-2xl p-8 mx-4 rounded-2xl dark:bg-gray-800 bg-primary/10 relative">
                       <img
                         src={img}
                         alt=""
                         className="rounded-full block mx-auto"
                       />
-                      <h1 className="text-2xl font-bold pb-4">{name}</h1>
+                      <h1 className="text-2xl font-semibold pb-8">{name}</h1>
                       <p className="text-gray-600 text-md text-justify">{text}</p>
-                      <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">
+                      <p className="text-black/40 text-9xl font-serif absolute top-0 right-0">
                         ,,
                       </p>
                     </div>
--- client/src/pages/Contracts.jsx
+++ client/src/pages/Contracts.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import img1 from "../../src/assets/places/C1.jpg"
 import img2 from "../../src/assets/places/C2.jpg"
 import img3 from "../../src/assets/places/C3.jpg"
@@ -7,9 +6,9 @@ import img4 from "../../src/assets/places/C4.jpg"
 const Contracts = () => {
   return (
     <>
-    <div className="dark:bg-gray-900 dark:text-white bg-gray-50 py-10 pt-[100px] md:pt-[160px]">
+    <div className="dark:bg-gray-900 dark:text-white bg-gray-50 py-10 pt-[100px] md:pt-[180px]">
       <section data-aos="fade-up" className="container ">
-        <h1 className=" my-8 border-l-8 border-primary/80 py-2 pl-2 text-3xl font-bold font-serif md:text-4xl">
+        <h1 className=" my-8 border-l-8 border-primary/80 py-6 pl-2 text-3xl font-bold font-mono md:text-6xl">
         Our BPO Contracts
         </h1>
         
@@ -22,16 +21,16 @@ const Contracts = () => {
               <img
                 src={img1}
                 alt="No image"
-                className="mx-auto h-[380px] w-full object-cover transition duration-700 hover:skew-x-2 hover:scale-110 rounded-[22px]"
+                className="mx-auto h-[450px] p-4 w-full object-cover transition duration-700 hover:skew-x-2 hover:scale-110 rounded-[30px]"
               />
             </div>
     
             <div className="space-y-2 p-3">
-              <h1 className="line-clamp-1 font-semibold text-xl pt-4 pb-3">Fixed-Term Contracts </h1>
+              <h1 className="line-clamp-1 font-semibold text-xl pt-4 pl-2 pb-3">Fixed-Term Contracts </h1>
               <div className="flex items-center gap-3 opacity-60">
               
-                <span>These contracts are established for a specific duration, typically outlining the client's service requirements and the terms for a set period (e.g., 6 months, 1 year).
-                <br/>
+                <span className="text-justify p-2">These contracts are established for a specific duration, typically outlining the client's service requirements and the terms for a set period (e.g., 6 months, 1 year).
+                <br/><br/>
                 Fixed-term contracts are useful for projects with clearly defined scopes or needs, such as seasonal demand or specific project initiatives. Once the period ends, the contract may be renewed or terminated based on performance and business needs.</span>
                 </div>
               
@@ -45,16 +44,16 @@ const Contracts = () => {
               <img
                 src={img2}
                 alt="No image"
-                className="mx-auto h-[380px] w-full object-cover transition duration-700 hover:skew-x-2 hover:scale-110 rounded-[22px]"
+                className="mx-auto  h-[450px] p-4 w-full object-cover transition duration-700 hover:skew-x-2 hover:scale-110 rounded-[30px]"
               />
             </div>
     
             <div className="space-y-2 p-3">
-              <h1 className="line-clamp-1 font-semibold text-xl pt-4 pb-3">Temporary Project or Job-Based Contracts </h1>
+              <h1 className="line-clamp-1 font-semibold text-xl pt-4 pl-2 pb-3">Temporary Project or Job-Based Contracts </h1>
               <div className="flex items-center gap-3 opacity-60">
               
-                <span>These contracts are designed for specific projects or tasks that have a defined start and end date. They often involve short-term work such as seasonal customer support during peak periods or specific project-based tasks.
-                <br/>
+                <span className="text-justify p-2">These contracts are designed for specific projects or tasks that have a defined start and end date. They often involve short-term work such as seasonal customer support during peak periods or specific project-based tasks.
+                <br/><br/>
                 Ideal for businesses needing additional resources for short durations without committing to long-term contracts. These contracts can help organizations manage flexibility in staffing according to fluctuating business demands.</span>
                 </div>
               
@@ -68,16 +67,16 @@ const Contracts = () => {
               <img
                 src={img3}
                 alt="No image"
-                className="mx-auto h-[380px] w-full object-cover transition duration-700 hover:skew-x-2 hover:scale-110 rounded-[22px]"
+                className="mx-auto  h-[450px] p-4 w-full object-cover transition duration-700 hover:skew-x-2 hover:scale-110 rounded-[30px]"
               />
             </div>
     
             <div className="space-y-2 p-3">
-              <h1 className="line-clamp-1 font-semibold text-xl pt-4 pb-3">Contracts with No Mandatory Contract Period</h1>
+              <h1 className="line-clamp-1 font-semibold text-xl pt-4 pl-2pb-3">Contracts with No Mandatory Contract Period</h1>
               <div className="flex items-center gap-3 opacity-60">
               
-              <span>These contracts do not impose a set duration for the agreement, allowing for flexibility. They can be utilized on an as-needed basis without being tied to a specific timeline.
-                <br/>
+              <span className="text-justify p-2">These contracts do not impose a set duration for the agreement, allowing for flexibility. They can be utilized on an as-needed basis without being tied to a specific timeline.
+                <br/><br/>
                 This type is appropriate for ongoing services where the relationship can evolve. Businesses can terminate the service with proper notice without a rigid contract period.</span>
                 </div>
               
@@ -92,16 +91,16 @@ const Contracts = () => {
               <img
                 src={img4}
                 alt="No image"
-                className="mx-auto h-[380px] w-full object-cover transition duration-700 hover:skew-x-2 hover:scale-110 rounded-[22px]"
+                className="mx-auto  h-[450px] p-4 w-full object-cover transition duration-700 hover:skew-x-2 hover:scale-110 rounded-[30px]"
               />
             </div>
     
             <div className="space-y-2 p-3">
-              <h1 className="line-clamp-1 font-semibold text-xl pt-4 pb-3">No Obligatory Contracts</h1>
+              <h1 className="line-clamp-1 font-semibold text-xl pt-4 pl-2 pb-3">No Obligatory Contracts</h1>
               <div className="flex items-center gap-3 opacity-60">
               
-              <span>These are informal agreements that do not require the signing of a formal contract, leading to a flexible working arrangement. They are often based on mutual understanding and can be in the form of service requests or purchase orders.
-                <br/>
+              <span className="text-justify p-2">These are informal agreements that do not require the signing of a formal contract, leading to a flexible working arrangement. They are often based on mutual understanding and can be in the form of service requests or purchase orders.
+                <br/><br/>
                 Suitable for organizations looking for a low-commitment approach, often used for trial periods or initial engagements before entering into more formal agreements.</span>
                 </div>
               
