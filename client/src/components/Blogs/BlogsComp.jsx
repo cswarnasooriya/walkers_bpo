4839be160f9bec27d88db555e5344ce159816415
Layani-Wickramaarachchi <layanianuththara20@gmail.com>
2024-10-25 10:44:31 -0700

    Services Updated

--- client/src/components/Blogs/BlogsComp.jsx
+++ client/src/components/Blogs/BlogsComp.jsx
@@ -42,7 +42,7 @@ Enter date one section finance */}
                 <h1 className="line-clamp-1 font-bold text-2xl pb-4">Finance & Accounting</h1>
 
                 <p className="pt-2 text-[17px] line-clamp-2 text-gray-600 font-serif text-justify leading-7">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi assumenda alias facere maxime optio! Hic fugiat maiores aliquam natus molestias eaque iure? Pariatur, sapiente dicta vel autem consequuntur consectetur eum!
+                In today’s fast-paced business environment, managing finances efficiently is crucial for success. Our Finance and Accounting BPO services provide businesses with the expertise and technology to streamline operations, enhance accuracy, and ensure compliance. 
                 </p>
 
               </div>
@@ -50,8 +50,8 @@ Enter date one section finance */}
               <div>
                 <Link to={"/finance"}>
                 <button className="bg-blue-300 mt-4 px-5 py-3 rounded-md text-black font-semibold hover:bg-blue-700 hover:text-white sm:ml-10 md:ml-32 lg:ml-40 xl:ml-52 2xl:ml-64 ml-40 items-center">
-  Read More!
-</button>
+                   Read More!
+                </button>
 
                 </Link>
               </div>
@@ -69,7 +69,7 @@ Enter date one section finance */}
                 <h1 className="line-clamp-1 font-bold text-2xl pb-4">IT Support & Data Entry </h1>      
 
                 <p className="pt-2 text-[17px] line-clamp-2 text-gray-600 font-serif text-justify leading-7">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi assumenda alias facere maxime optio! Hic fugiat maiores aliquam natus molestias eaque iure? Pariatur, sapiente dicta vel autem consequuntur consectetur eum!
+                In the current world where everything is moving so fast especially with the introduction of technology it is important for a business to have good IT support and proper handling of data. 
                 </p>        
               </div>
 
@@ -94,7 +94,7 @@ Enter date one section finance */}
               <div className="space-y-2 py-3">
                 <h1 className="line-clamp-1 font-bold text-2xl pb-4">Digital Solutions</h1>            
                 <p className="pt-2 text-[17px] line-clamp-2 text-gray-600 font-serif text-justify leading-7">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi assumenda alias facere maxime optio! Hic fugiat maiores aliquam natus molestias eaque iure? Pariatur, sapiente dicta vel autem consequuntur consectetur eum!
+                Expertly crafting websites with creative design, optimizing visibility through digital marketing and SEO, and delivering user-friendly UX/UI, all focused on enhancing your brand's success.
                 </p> 
               </div>
 
@@ -116,7 +116,7 @@ Enter date one section finance */}
               <div className="space-y-2 py-3">
                 <h1 className="line-clamp-1 font-bold text-2xl pb-4">Project Management</h1>
                 <p className="pt-2 text-[17px] line-clamp-2 text-gray-600 font-serif text-justify leading-7">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi assumenda alias facere maxime optio! Hic fugiat maiores aliquam natus molestias eaque iure? Pariatur, sapiente dicta vel autem consequuntur consectetur eum!
+                At Walkers BPO, our estimating services are tailored to support your projects efficiently within the manufacturing sector.
                 </p>
               </div>
               <div>
@@ -136,10 +136,10 @@ Enter date one section finance */}
                 />
               </div>
               <div className="space-y-2 py-3">
-                <h1 className="lline-clamp-1 font-bold text-2xl pb-4">Engineering Design and Drafting</h1>
+                <h1 className="lline-clamp-1 font-bold text-2xl pb-4">Engineering Design, Drafting, and Estimating in Austrailia </h1>
 
                 <p className="pt-2 text-[17px] line-clamp-2 text-gray-600 font-serif text-justify leading-7">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi assumenda alias facere maxime optio! Hic fugiat maiores aliquam natus molestias eaque iure? Pariatur, sapiente dicta vel autem consequuntur consectetur eum!
+                In the ever-evolving landscape of engineering, efficiency and precision are paramount. Walkers BPO specializes in providing comprehensive Business Process Outsourcing (BPO) support services dedicated to engineering design, drafting, and estimating.
                 </p>
               </div>
               <div>
@@ -148,28 +148,8 @@ Enter date one section finance */}
                 </Link>
               </div>
         </div>
-        <div className="p-6 shadow-lg transition-all duration-500 hover:shadow-xl dark:bg-slate-950 dark:text-white rounded-2xl">
-              <div className="overflow-hidden">
-                <img
-                  src={Img4}
-                  alt="No image"
-                  className="mx-auto h-[450px] w-full object-cover transition duration-700 hover:skew-x-2 hover:scale-110 rounded-xl"
-                />
-              </div>
-              <div className="space-y-2 py-3">
-                <h1 className="line-clamp-1 font-bold text-2xl pb-4">Estimating Services in Austrailia</h1>
-                <p className="pt-2 text-[17px] line-clamp-2 text-gray-600 font-serif text-justify leading-7">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi assumenda alias facere maxime optio! Hic fugiat maiores aliquam natus molestias eaque iure? Pariatur, sapiente dicta vel autem consequuntur consectetur eum!
-                </p>
-              </div>
-              <div>
-                <Link to={"/project"}>
-                  <button className="bg-blue-300 mt-4 px-5 py-3 rounded-md text-black font-semibold hover:bg-blue-700 hover:text-white sm:ml-10 md:ml-32 lg:ml-40 ml-40 xl:ml-52 2xl:ml-64 items-center">Read More!</button>
-                </Link>
-              </div>
-        </div>
-
-
+        
+        
               </div>
                         </section>
                       </div>
